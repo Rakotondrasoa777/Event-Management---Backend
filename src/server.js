1640d43476cc43adb27574466e7f2e8e89a0f516
Donner <152691192+DOnn3r@users.noreply.github.com>
2025-03-15 08:19:27 +0300

    fix add cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const eventController = require("./controllers/showEvent");
 const userControlllers = require("./controllers/showUser");
 
 const app = express();
 const port = 3000
 
+app.use(cors({
+    origin: 'http://localhost:5173', // Autoriser uniquement ce domaine
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes autorisées
+}));
+
 app.get('/events', eventController.getAllEvent);
 app.get('/users', userControlllers.getAllUser);
+
 
 app.listen(port, () => {
     console.log(`Server is running in :  http://localhost:${port}`);
