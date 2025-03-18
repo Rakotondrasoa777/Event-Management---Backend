fbc162ecf85b8d65b0fabcfdf33d6bdef91ddfb6
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-03-18 19:11:50 +0300

    feat: add new controller getEventById

--- src/controllers/showEvent.js
+++ src/controllers/showEvent.js
@@ -2,14 +2,40 @@ const pool = require('../config/db');
 
 const eventController = {
     getAllEvent: async(req, res) => {
+        let sql = "select * from event"
+
         try {
-            const {rows} = await pool.query('select * from event')
-            res.json(rows);
+            const {rows} = await pool.query(sql)
+            res.status(200).json(rows);
         } catch (e) {
             console.error(e);
             res.status(500).send('Server error')
         }
+    },
+
+    getEventById: async(req, res) => {
+        const id = parseInt(req.params.id);
+        let sql = "select * from event where id = $1"
+        
+        if (isNaN(id)) {
+            return res.status(400).send('Invalid ID');
+        }
+    
+        try {
+            const {rows} = await pool.query(sql, [id])
+
+            if(rows.length === 0) {
+                return res.status(404).send('Event not found');
+            }
+
+            res.status(200).json(rows);
+        } catch (e) {
+            console.error(e)
+            res.status(500).send('Server error')
+        }
     }
+
+
 }
 
 module.exports = eventController
