ce6ea0a9e2d746590c4680bfb7a0e39ccc0eb545
KareeemNomenjanahary12 <hei.nomena.2@gmail.com>
2025-03-22 09:12:31 +0300

    feat: add controllers

--- src/controllers/showUser.js
+++ src/controllers/showUser.js
@@ -1,6 +1,6 @@
 const pool = require("../config/db");
 
-const userControlllers = {
+const userControllers = {
     getAllUser: async(req, res) => {
         try {
             const {rows} = await pool.query('select * from \"user\"')
@@ -9,7 +9,29 @@ const userControlllers = {
             console.error(e);
             res.status(500).send('Server error')
         }
-    }
+    },
+
+    getUserById: async(req, res) => {
+        const id = parseInt(req.params.id);
+        let sql = "select * from \"user\" where id = $1"
+
+        if (isNaN(id)) {
+            return res.status(400).send('Invalid ID');
+        }
+    
+        try {
+            const {rows} = await pool.query(sql, [id])
+
+            if(rows.length === 0) {
+                return res.status(404).send('Event not found');
+            }
+
+            res.status(200).json(rows);
+        } catch (e) {
+            console.error(e)
+            res.status(500).send('Server error')
+        }
+    },
 }
 
-module.exports = userControlllers
+module.exports = userControllers
