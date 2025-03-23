5ea11d69f446d5991464dea163401684d88a0a28
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-03-23 21:38:47 +0300

    feat: add register and login controller for authentification

---
+++ src/controllers/authController.js
@@ -0,0 +1,48 @@
+
+const User = require("../models/User");
+const generateToken = require("../utils/generateToken");
+const bcrypt = require('bcryptjs');
+
+const authController = {
+    register: async (req, res) => {
+
+        const { username, email, password } = req.body;
+        try {
+            const existingUser = await User.findUserByUsername(username);
+            if (existingUser) {
+                return res.status(400).json({ error: 'User already exists' });
+            }
+
+            const user = await User.createUser(username, email, password);
+            const token = await generateToken(user.username);
+            res.status(201).send({token});
+        } catch(e) {
+            res.status(400).json({error: e.message})
+        }
+    },
+    
+    login: async (req, res) => {
+        
+        const { username, password } = req.body;
+    
+        try {
+            const user = await User.findUserByUsername(username);
+            console.log(user);
+            
+                if(!user) {
+                    return res.status(404).send({error: "User not found"})
+                }
+            
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({error: "Wrong password"});
+            }
+            const token = generateToken(user.username);
+            res.status(200).json({token});
+        } catch (e) {
+            res.status(400).json({error: e.message})
+        }
+    }
+}
+
+module.exports = authController;
