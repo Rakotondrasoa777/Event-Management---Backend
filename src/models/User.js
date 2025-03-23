8eb94bfe5cb51074878542b933e265d9c6e2301c
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-03-23 21:45:35 +0300

    feat: add model for creation or find by username for users

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const pool = require("../config/db");
+const bcrypt = require('bcryptjs');
+
+class User {
+    static async createUser(username, email, password) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const createUserSql = "insert into users (username, email, password) values ($1, $2, $3)";
+        const values = [username, email, hashedPassword]
+        const {rows} = await pool.query(createUserSql, values);
+        return rows;
+    }
+
+    static async findUserByUsername(username) {
+        const findUserSql = "select id, username, email, password from users where username = $1";
+        const {rows} = await pool.query(findUserSql, [username]);
+        
+        return rows[0];
+    }
+}
+
+module.exports = User;
