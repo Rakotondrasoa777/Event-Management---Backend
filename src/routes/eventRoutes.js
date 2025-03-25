66cb87c66968c9818fab7662bc9659cc93160b90
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-03-25 16:46:07 +0300

    feat: add new route for creating, updating and deleting event

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -6,5 +6,8 @@ const eventController = require("../controllers/showEvent");
 router.get("/", eventController.getAllEvent);
 router.get("/:id", eventController.getEventById);
 router.get("/date/:date", eventController.filterEventsByDate);
+router.post("/create", eventController.createEvent);
+router.put("/update/:id", eventController.updateEventById);
+router.delete("/delete/:id", eventController.deleteEventById);
 
 module.exports = router;
