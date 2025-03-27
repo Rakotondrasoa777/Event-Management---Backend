a840da4865950e2679e1bce102e0fdae514a259f
KareeemNomenjanahary12 <hei.nomena.2@gmail.com>
2025-03-27 10:42:01 +0300

    feat: post, put and delete routes added to userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 router.get("/",userControllers.getAllUser);
 router.get("/:id",userControllers.getUserById);
+router.post("/create", userControllers.createUser);
+router.put("/update/:id", userControllers.updateUserById);
+router.delete("/delete/:id", userControllers.deleteUserById);
 
 
 module.exports = router;
