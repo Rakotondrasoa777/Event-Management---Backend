a28c60a3e53b83a3e4bfd69d916a266d39585453
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2025-04-04 11:36:40 +0300

    fix: change route of reservation of user

--- src/routes/reservationRoutes.js
+++ src/routes/reservationRoutes.js
@@ -3,7 +3,7 @@ const reservationController = require("../controllers/reservationController");
 const router = express.Router();
 
 router.post("/reserve", reservationController.reserveEvent);
-router.get("/list", reservationController.showReserveOfUser);
+router.get("/list/:username", reservationController.showReserveOfUser);
 router.get("/income", reservationController.getMonthlyIncomeStats);
 router.get("/users/list", reservationController.showUsersReservation);
 router.get("/stats/ticket-types", reservationController.statsTicketTypes);
